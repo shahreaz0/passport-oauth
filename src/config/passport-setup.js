3d1dc976692f5378171a9f1b7dee4213cfb48157
shahreaz0 <ashahreaz@gmail.com>
2020-06-01 17:41:44 +0600

    Google strategy setup

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -1,4 +1,13 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
-passport.use(new GoogleStrategy({}, () => {}));
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET,
+		},
+		() => {},
+	),
+);
