5936003cd949e26516088ddfce829c4c97d83648
shahreaz0 <ashahreaz@gmail.com>
2020-06-24 04:03:33 +0600

    Save user to mongodb atlas

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
 require("dotenv").config();
 
 passport.use(
@@ -9,8 +10,19 @@ passport.use(
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 			callbackURL: "/auth/google/cb",
 		},
-		(accessToken, refreshToken, profile, cb) => {
+		async (accessToken, refreshToken, profile, cb) => {
 			console.log(profile);
+			try {
+				const user = new User({
+					username: profile.displayName,
+					googleId: profile.id,
+				});
+
+				await user.save();
+				console.log(user);
+			} catch (err) {
+				console.log(err);
+			}
 		},
 	),
 );
