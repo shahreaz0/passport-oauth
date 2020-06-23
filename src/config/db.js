cfa3e2b8bf0ec6d53a01bd560c73b9911320ce9b
shahreaz0 <ashahreaz@gmail.com>
2020-06-24 02:22:45 +0600

    User model created

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,15 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const mongoConnect = async () => {
-	await mongoose.connect(process.env.MONGO_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
-
-	console.log("Mongodb connected!!");
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log("Mongodb connected!!");
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 mongoConnect();
